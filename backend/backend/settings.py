3b0f742dd61433ed839cb0f115c8aad7c708f2a6
Carlos <carlosmanuelibarra11@gmail>
2024-07-29 11:34:50 -0500

    increasing token life time

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,8 +121,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),  
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=180),  
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
@@ -148,8 +148,8 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=7),  
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=30),  
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
